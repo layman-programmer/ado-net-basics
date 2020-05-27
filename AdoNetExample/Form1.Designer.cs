247e24501e535a48eb21b77fbb8216faedcfe5f0
Mata Prasad Chauhan [MacbookBootcamp] <mataprasad045@gmail.com>
2020-05-27 20:32:12 +0530

    Part1

---
+++ AdoNetExample/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace AdoNetExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowEmpData = new System.Windows.Forms.Button();
+            this.lbEmpData = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnShowEmpData
+            // 
+            this.btnShowEmpData.Location = new System.Drawing.Point(12, 12);
+            this.btnShowEmpData.Name = "btnShowEmpData";
+            this.btnShowEmpData.Size = new System.Drawing.Size(143, 23);
+            this.btnShowEmpData.TabIndex = 0;
+            this.btnShowEmpData.Text = "Show Employee Data";
+            this.btnShowEmpData.UseVisualStyleBackColor = true;
+            this.btnShowEmpData.Click += new System.EventHandler(this.btnShowEmpData_Click);
+            // 
+            // lbEmpData
+            // 
+            this.lbEmpData.FormattingEnabled = true;
+            this.lbEmpData.Location = new System.Drawing.Point(12, 71);
+            this.lbEmpData.Name = "lbEmpData";
+            this.lbEmpData.Size = new System.Drawing.Size(316, 368);
+            this.lbEmpData.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 450);
+            this.Controls.Add(this.lbEmpData);
+            this.Controls.Add(this.btnShowEmpData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowEmpData;
+        private System.Windows.Forms.ListBox lbEmpData;
+    }
+}
+
---
+++ AdoNetExample/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdoNetExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowEmpData_Click(object sender, EventArgs e)
+        {
+            var connectionString = new SqlConnectionStringBuilder();
+            connectionString.DataSource = "DESKTOP-OGMGVV9\\SQLEXPRESS";
+            connectionString.InitialCatalog = "DbScott";
+            connectionString.IntegratedSecurity = true;
+            using (var con = new SqlConnection(connectionString.ToString())) // 1) get the connection
+            {
+                con.Open(); // 2) open the connection
+                var cmd = new SqlCommand(); // 3) prepare the command
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT * FROM EMP;";
+
+                using (var reader = cmd.ExecuteReader()) // 4) execute the command
+                {
+                    this.ShowData(reader);
+                    con.Close(); // 5) close the connection
+                }
+            }
+        }
+
+        private void ShowData(SqlDataReader reader)
+        {
+            lbEmpData.Items.Clear();
+            while (reader.Read())
+            {
+                var item = String.Format("#{0} - {1}",
+                    Convert.ToInt32(reader["EMPNO"]),
+                    Convert.ToString(reader["ENAME"]));
+                lbEmpData.Items.Add(item);
+            }
+        }
+    }
+}
